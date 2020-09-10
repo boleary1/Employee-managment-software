09e318991a89de94b0686b024adccda17533d23d
Brian OLeary <oleary.brian1@gmail.com>
2020-09-09 22:06:51 -0400

    needs mysql2

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-var mysql = require("mysql");
+var mysql = require("mysql2");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
 
     // Your port; if not 3306
     port: 3306,
     user: "root",
     password: "root",
-    database: "employee_listdb"
+    database: "employee_listDB"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    init();
-});
+
 
 function init() {
     console.log("Welcome to the Employee managment software!")
-    inquirer.prompt([
-            {
-                type: 'list',
-                name: 'reptile',
-                message: 'Which is better?',
-                choices: ['alligator', 'crocodile'],
-            },
-        ])
+    inquirer
         .prompt({
             name: "action",
             type: "list",
@@ -40,40 +30,47 @@ function init() {
                 "Update an employee's role?",
             ]
         })
-    // .then(function (answer) {
-    //     switch (answer.action) {
-    //         case "View all departments?":
-    //             viewDepartments();
-    //             break;
+    .then(function (answer) {
+        console.log(answer)
+        switch (answer.action) {
+            case "View all departments?":
+                
+                viewDepartments();
+                break;
 
-    //         case "Add a department?":
-    //             addDepartments();
-    //             break;
+            case "Add a department?":
+                addDepartments();
+                break;
 
-    //         case "View all roles?":
-    //             viewRoles();
-    //             break;
+            case "View all roles?":
+                viewRoles();
+                break;
 
-    //         case "Add a role?":
-    //             addRole();
-    //             break;
+            case "Add a role?":
+                addRole();
+                break;
 
-    //         case "View employee list?":
-    //             viewEmployees();
-    //             break;
+            case "View employee list?":
+                viewEmployees();
+                break;
 
-    //         case "Add an employee?":
-    //             addEmployee();
-    //             break;
+            case "Add an employee?":
+                addEmployee();
+                break;
 
-    //         case "Update an employee's role?":
-    //             updateRole();
-    //             break;
-    //     }
-    // });
+            case "Update an employee's role?":
+                updateRole();
+                break;
+        }
+    });
 }
 
-init();
+connection.connect(function (err) {
+    if (err) console.log(err);
+    init();
+});
+
+
 function viewDepartments() {
     console.log()
 };
